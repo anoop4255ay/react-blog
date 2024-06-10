58e2e45b8d16e8ac594b2492119f40c018c0e063
Anoop Yadav <139114879+anoop4255ay@users.noreply.github.com>
2024-06-10 15:45:26 +0530

    Update App.js

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-
+<import Post from'./component/Post'
 function App() {
   return (
    <>
    <h1>Welcome ANOOP YADAV in React with Github😍😘🥰🤩</h1>
+    <Post/>
    </>
   );
 }
